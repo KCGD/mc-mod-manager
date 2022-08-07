89be71dcc6651a0d472d066548c144bdc10955e8
KCGD <kcgdhosting@gmail.com>
2022-08-07 00:18:21 -0400

    added local repo support

--- config.ts
+++ config.ts
@@ -1,1 +1,5 @@
-export const RepoList:string[] = [];
+export const RepoList:string[] = [
+    "https://github.com/KCGD/modpack-repository",
+    "local"
+];
+export const parralelDownloads:number = 4;
--- main.ts
+++ main.ts
@@ -10,9 +10,15 @@ let keypress = require('keypress');
 import * as fs from "fs";
 import * as path from "path";
 import * as process from "process";
+import * as config from "./config";
 import * as readline from "readline";
-import { getAppdataPath } from "./src/modules/appdataPath";
 import { createMenu } from "./src/modules/selectionMenu";
+import type { menuObject } from "./src/modules/selectionMenu";
+import { getLocalRepo } from "./src/modules/localModRepo";
+import type { RepoData } from "./src/modules/localModRepo";
+import { getAppdataPath } from "./src/modules/appdataPath";
+import { recurse } from "./src/modules/recursion";
+
 
 //create template for user argument parsing
 //only flags that require aditional arguments will be assigned here
@@ -77,20 +83,58 @@ function Main(): void {
         if(fs.existsSync(path.join(mcPath, "mods/"))) {
             if(fs.readdirSync(path.join(mcPath, "mods/")).length > 0) {
                 //mods present, ask to make backup, then select a modpack
-                selectModpack(mcPath);
+                getRepoInfo(path.join(mcPath, "modBackupRepo"), mcPath);
             } else {
                 //select a modpack
-                selectModpack(mcPath);
+                getRepoInfo(path.join(mcPath, "modBackupRepo"), mcPath);
             }
         } else {
             //select a modpack
-            selectModpack(mcPath);
+            getRepoInfo(path.join(mcPath, "modBackupRepo"), mcPath);
         }
     }
+    //pull index from the repo (special case for local repo)
+    function getRepoInfo(modRepoPath:string, mcPath:string): void {
+        let menuItems:menuObject[] = [];
+        recurse(config.RepoList, function(repoType:string, next:Function) {
+            if(repoType === "local") {
+                getLocalRepo(modRepoPath, function(repoData, error): void {
+                    if(!error && repoData) {
+                        //merge into menu list
+                        for(var j = 0; j < repoData.length; j++) {
+                            menuItems.push({
+                                "path":path.join(modRepoPath, repoData[j].path),
+                                "type":"local",
+                                "version":repoData[j].version,
+                                "name":repoData[j].name
+                            })
+                        }
+                        next();
+                    } else {
+                        switch(error) {
+                            case "ERR_NO_REPO":
+                                throw new Error (error);
+                            break;
+                            default:
+                                throw new Error(error);
+                            break;
+                        }
+                    }
+                })
+            } else {
+                //git repo
+                next();
+            }
+        }, function() {
+            selectModpack(mcPath, menuItems);
+        })
+    }
     //select a modpack
-    function selectModpack(mcPath:string): void {
-        createMenu(Array.apply(null, Array(20)).map(function(x, i) {return `Modpack ${i}`}), function(selection:string): void {
-            console.log(`Selected: ${selection}`);
+    function selectModpack(mcPath:string, menu:menuObject[]): void {
+        console.log(menu);
+        //test modpack function: Array.apply(null, Array(20)).map(function(x, i) {return `Modpack ${i}`})
+        createMenu(menu, function(selection:menuObject): void {
+            console.log(selection);
         }, function(): void {
             console.log(`Menu canceled`);
         }, "Please select a modpack (use the up/down arrow to navigate, and return to select):",
