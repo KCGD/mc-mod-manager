786fea9ce667fd6cdfffbfa4a83b7cf7e42feb9e
KCGD <kcgdhosting@gmail.com>
2022-08-29 18:22:46 -0400

    added unzip function to zip module

--- src/modules/zipFiles.ts
+++ src/modules/zipFiles.ts
@@ -3,6 +3,11 @@ import * as path from "path";
 import * as yazl from 'yazl';
 import { recurse } from "./recursion";
 import type { ZipFile } from "yazl";
+import * as yauzl from "yauzl";
+
+interface unzipCallback {
+    (error:Error|undefined): void
+}
 
 export function ZipDir(rootPath:string, files:string[], output:string, callback:Function): void {
     let zip = new yazl.ZipFile();
@@ -35,4 +40,44 @@ export function ZipDir(rootPath:string, files:string[], output:string, callback:
             callback();
         })
     }
+}
+
+export function Unzip(zipFilePath:string, outputDirectory:string, callback:unzipCallback) {
+    yauzl.open(zipFilePath, {lazyEntries: true}, function(error, zipFile): void {
+        //recursively unzip (recursion built into yauzl)
+        if(error) {
+            throw error;
+        }
+        zipFile.readEntry();
+        //called automatically once zip file completed
+        zipFile.once('end', function(): void {
+            zipFile.close();
+            callback(undefined);
+        })
+        zipFile.on("entry", function(entree): void {
+            //if directory (in zip) is a directory, run mkdir, else continue recursion
+            if(/\/$/.test(entree.fileName)) {
+                fs.mkdir(path.join(outputDirectory, entree.fileName), function(error): void {
+                    if(error) {
+                        callback(error);
+                    } else {
+                        zipFile.readEntry();
+                    }
+                })
+            } else {
+                let entreeWriteStream:fs.WriteStream = fs.createWriteStream(path.join(outputDirectory, entree.fileName));
+                zipFile.openReadStream(entree, function(error, readStream): void {
+                    if(error) {
+                        callback(error);
+                    } else {
+                        readStream.pipe(entreeWriteStream);
+                        readStream.on('end', function(): void {
+                            entreeWriteStream.close();
+                            zipFile.readEntry();
+                        })
+                    }
+                })
+            }
+        })
+    })
 }
